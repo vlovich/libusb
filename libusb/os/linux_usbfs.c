4954673c4baf6dcfe2361f38347c03d7d3cd2c1f
Vitali Lovich <vlovich@gmail.com>
2011-08-20 12:41:33 -0700

    libusb_port_path on linux

--- libusb/os/linux_usbfs.c
+++ libusb/os/linux_usbfs.c
@@ -2243,6 +2243,42 @@ static int op_clock_gettime(int clk_id, struct timespec *tp)
   }
 }
 
+static int op_sysfspath(struct libusb_device *dev, char *path, size_t *pathlen)
+{
+	int fd = __open_sysfs_attr(dev, "descriptors");
+	size_t sysfslen;
+	struct linux_device_priv *priv = __device_priv(dev);
+	int r = LIBUSB_SUCCESS;
+
+	if (fd < 0) {
+		if (errno == ENOENT) {
+			r = LIBUSB_ERROR_NO_DEVICE;
+			goto out;
+		}
+
+		r = LIBUSB_ERROR_OTHER;
+
+		usbi_warn(DEVICE_CTX(dev),
+			"cannot open descriptors because r=%d,errno=%d", r, errno);
+		goto out;
+	}
+	close(fd);
+
+	sysfslen = strlen(priv->sysfs_dir);
+
+	strncpy(path, priv->sysfs_dir, *pathlen);
+	path[*pathlen] = 0;
+
+	if (sysfslen > *pathlen) {
+		r = LIBUSB_ERROR_OVERFLOW;
+	}
+
+	*pathlen = sysfslen;
+
+out:
+	return r;
+}
+
 #ifdef USBI_TIMERFD_AVAILABLE
 static clockid_t op_get_timerfd_clockid(void)
 {
@@ -2285,7 +2321,7 @@ const struct usbi_os_backend linux_usbfs_backend = {
 
 	.clock_gettime = op_clock_gettime,
 
-	.get_portpath = NULL,
+	.get_portpath = op_sysfspath,
 
 #ifdef USBI_TIMERFD_AVAILABLE
 	.get_timerfd_clockid = op_get_timerfd_clockid,
